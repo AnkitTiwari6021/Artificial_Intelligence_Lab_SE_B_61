d81c23d6877b522f8ea72d2c1efca502faeb1f59
AnkitTiwari6021 <ankit.tiwari24@mmit.edu.in>
2025-08-14 15:49:50 +0530

    it's create

---
+++ Artificial_Intelligence_Lab/program2.py
@@ -0,0 +1,12 @@
+sub1 = input("\n Enter the subject of your choice : ")
+sub2 = input("\n Enter the subject of your choice : ")
+
+
+if(sub1 == "maths" and  sub2 == "physics") or (sub1 == "physics" and  sub2 == "maths"):
+   print("mechanical enginnering")
+ 
+if(sub1 == "programing" and sub2 == "maths") or (sub1 == "maths" and sub2 == "programing"):
+   print("computer enginnering")
+ 
+if(sub1 == "programming" and sub2 == "statics") or (sub1 == "statics" and sub2 == "programming"):
+    print("artificial intelligence & data science")
